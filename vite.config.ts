4b2b926f8e4c3ad08b4124d567725b73fd238427
Hauke Baasch <h.baasch@reply.de>
2024-07-07 10:15:05 +0200

    removed base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://bashtec.github.io/klaus-franzreb/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
